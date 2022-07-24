ec593d91bf942ed2589ae630730a4f7b28c8f7ac
qingyanmaodai <923340767@qq.com>
2022-07-24 20:38:43 +0800

    .

---
+++ src/views/Other/pageOne.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是page1管理
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pageOne",
+  data() {
+    return {
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Other/pageTwo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是page2管理
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageTwo",
+  data() {
+    return {
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
